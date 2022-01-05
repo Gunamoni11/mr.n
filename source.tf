f979c4ce6f13b80655719a27624acba4968c4554
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-05 22:12:10 +0530

    Update source.tf

--- source.tf
+++ source.tf
@@ -1,6 +1,6 @@
 module "lb"{
-    source = "git::https://github.com/Gunamoni11/ec2.git"
+    source = "git::https://github.com/Gunamoni11/lb1.git"
 }
 module "asg"{
-    source = "git::https://github.com/Gunamoni11/ec2.git"
+    source = "git::https://github.com/Gunamoni11/asg.git"
 }
